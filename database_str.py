4523589f2e4941f3325c51b7353d0d2062fe5494
Quoc Manh <hoangquocmanh2004@gmail.com>
2024-11-09 15:12:46 +0700

    first commit

---
+++ database_str.py
@@ -0,0 +1,7 @@
+class Database_str:
+    def __init__(self):
+        self.host='localhost'
+        self.user='root'
+        self.password='mat khau sql cua ban'
+        self.database='manh'
+        self.port='3306'
